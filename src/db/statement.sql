40862498ac209e982dd88d359481c6458de9699c
ThapeloMas <thapelo0744@gmail.com>
2024-07-23 14:08:05 +0200

    first commit

---
+++ src/db/statement.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER
+)
+
+
+INSERT INTO users (name,age)
+VALUES
+    ('code',23),
+    ('tribe',23)
+
+    DROP TABLE users
